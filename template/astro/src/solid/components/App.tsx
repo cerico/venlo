5ffe43ce0f6ef9e3053c70f7a84eb4f1ec89e08e
cerico <garethrobertlee@gmail.com>
2023-08-12 13:10:40 +0200

    fix: correct placement of solidjs in default
    - corrects semantic bug with solidjs components inside inline element which prevents clicking

--- template/astro/src/solid/components/App.tsx
+++ template/astro/src/solid/components/App.tsx
@@ -4,8 +4,14 @@ function Header() {
   function increment() {
     store.setScore({ ...store.score(), current: 31 })
   }
+  function reset() {
+    store.setScore({ ...store.score(), current: 0 })
+  }
   return (
-    <span onClick={increment}>{store.score().current}</span>
+    <>
+    <div onClick={increment}>Current: {store.score().current}</div>
+    <button onClick={reset}>Reset</button>
+    </>
   )
 }
 
